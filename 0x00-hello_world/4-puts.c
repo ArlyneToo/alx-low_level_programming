b881a4b7f12c1114f788e5a153c908d2b5300622
Arlyne Too <too.arlyne@gmail.com>
2022-09-09 16:37:19 +0300

    a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -print
+ *
+ * Return: Always 0 (complete)
+ */
+int main () 
+{
+	puts("\n"programming is like building a multilingual puzzle");
+	return (0);
+} 
+
+
+         	
+
+
